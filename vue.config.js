08ab30f643d3043ead4e40e49eaebbc699b16e50
mac <mac@macdeMac-mini.lan>
2020-09-07 10:38:29 +0800

    vue-config.js

---
+++ vue.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  productionSourceMap: false, // 生产打包时不输出map文件，增加打包速度
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.warnings = false
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true
+      config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = [
+        'console.log'
+      ]
+    }
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('src', resolve('src'))
+      .set('common', resolve('src/common'))
+      .set('components', resolve('src/components'))
+    config.resolve.symlinks(true)
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+  }
+}
